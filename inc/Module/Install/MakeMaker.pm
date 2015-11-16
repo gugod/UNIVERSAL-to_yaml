750e17bdbf5df31dd02502b9c3608f288d4ef998
Kang-min Liu <gugod@gugod.org>
2015-11-16 22:03:53 +0100

    update inc/

--- inc/Module/Install/MakeMaker.pm
+++ inc/Module/Install/MakeMaker.pm
@@ -2,29 +2,30 @@
 package Module::Install::MakeMaker;
 
 use strict;
-use Module::Install::Base;
-use ExtUtils::MakeMaker ();
+use ExtUtils::MakeMaker   ();
+use Module::Install::Base ();
 
-use vars qw{$VERSION $ISCORE @ISA};
+use vars qw{$VERSION @ISA $ISCORE};
 BEGIN {
-	$VERSION = '0.64';
+	$VERSION = '1.16';
+	@ISA     = 'Module::Install::Base';
 	$ISCORE  = 1;
-	@ISA     = qw{Module::Install::Base};
 }
 
-my $makefile;
+my $makefile = undef;
+
 sub WriteMakefile {
     my ($self, %args) = @_;
     $makefile = $self->load('Makefile');
 
     # mapping between MakeMaker and META.yml keys
     $args{MODULE_NAME} = $args{NAME};
-    unless ($args{NAME} = $args{DISTNAME} or !$args{MODULE_NAME}) {
+    unless ( $args{NAME} = $args{DISTNAME} or ! $args{MODULE_NAME} ) {
         $args{NAME} = $args{MODULE_NAME};
         $args{NAME} =~ s/::/-/g;
     }
 
-    foreach my $key (qw(name module_name version version_from abstract author)) {
+    foreach my $key ( qw{name module_name version version_from abstract author installdirs} ) {
         my $value = delete($args{uc($key)}) or next;
         $self->$key($value);
     }
@@ -32,6 +33,12 @@ sub WriteMakefile {
     if (my $prereq = delete($args{PREREQ_PM})) {
         while (my($k,$v) = each %$prereq) {
             $self->requires($k,$v);
+        }
+    }
+
+    if (my $prereq = delete($args{BUILD_REQUIRES})) {
+        while (my($k,$v) = each %$prereq) {
+            $self->build_requires($k,$v);
         }
     }
 
